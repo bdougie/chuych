3e84443c564306317e30209f469b0719078fa0ae
Brianllamar <ilikerobot@gmail.com>
2014-02-22 21:48:44 -0500

    update to form and controller

--- app/controllers/church_controller.rb
+++ app/controllers/church_controller.rb
@@ -1,9 +1,25 @@
 class ChurchController < ApplicationController
   def index
   	@churches = Church.all
+
   end
 
+  def new
+  	@church = Church.new
+  end
+
+  def create
+  	@church = Church.new(params[:id])
+  	if @church.save  	
+  		redirect_to @church, notice: "Church was saved successfully."
+  	else
+  		flash[:error] = "Error creating church. Please try again."
+			render :new
+		end
+  end	
+
   def show
+    @topic = Church.find(params[:id])
   end
 
   def edit
