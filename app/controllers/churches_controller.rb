0eba870fff515d554e45f0c4aba1fc0408130ed1
Brianllamar <ilikerobot@gmail.com>
2014-02-22 22:44:10 -0500

    updated routes and view with plural

---
+++ app/controllers/churches_controller.rb
@@ -0,0 +1,27 @@
+class ChurchesController < ApplicationController
+  def index
+  	@churches = Church.all
+  end
+
+  def new
+  	@church = Church.new
+  end
+
+  def create
+  	@church = Church.new(params[:id])
+  	if @church.save  	
+  		redirect_to @church, notice: "Church was saved successfully."
+  	else
+  		flash[:error] = "Error creating church. Please try again."
+			render :index
+		end
+  end	
+
+  def show
+    @topic = Church.find(params[:id])
+  end
+
+  def edit
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Chuych::Application.routes.draw do
   
-  resources :church,  only: [:index, :create, :new, :show]
+  resources :churches,  only: [:index, :create, :new, :show]
 
   root :to => 'welcome#index'
 
