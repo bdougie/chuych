e314e7d03343b1e9ef5c0343e7b734276ecc433f
Brianllamar <ilikerobot@gmail.com>
2014-02-23 06:59:39 -0500

    implement devise mailers and send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Chuych::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'chuyh.herokuapp.com' }  
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
@@ -77,4 +79,6 @@ Chuych::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
 end
