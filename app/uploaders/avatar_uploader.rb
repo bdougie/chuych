7b736367a05345734c41d7867fdbc84deb9255c8
Brianllamar <ilikerobot@gmail.com>
2014-02-24 07:13:25 -0500

    updated uploader and edit registration

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,12 +2,12 @@
 
 class AvatarUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or MiniMagick support:
+
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Rails::Helper
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
---
+++ test.rb
@@ -0,0 +1,39 @@
+def square_nums(num)
+i = 1
+# i = x * y
+	while num > i
+
+		num = num - i
+		i == num
+
+		
+		puts num if (num % i)
+		
+	end
+
+
+end
+
+square_nums(100)
+
+
+
+# i = 0
+# numbers = []
+
+# while i < 6
+#   puts "At the top i is #{i}"
+#   numbers.push(i)
+
+#   i = i + 1
+#   puts "Numbers now: #{numbers}"
+#   puts "At the bottom i is #{i}"
+# end
+
+# puts "The numbers: "
+
+# for num in numbers
+#   puts num
+# end
+
+
