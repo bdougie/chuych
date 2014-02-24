3412d4b1e289a835c96498713dde6f1f51b3a856
Brianllamar <ilikerobot@gmail.com>
2014-02-23 19:05:00 -0500

    updated send grid mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development?
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => 'app22460347@heroku.com',
+    :password       => 'izllaxun',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
