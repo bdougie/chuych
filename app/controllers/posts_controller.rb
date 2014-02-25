1ed542cb478cf4ed48d6f44c5678edc8192679a3
Brianllamar <ilikerobot@gmail.com>
2014-02-25 06:57:58 -0500

    post controller added

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+	def new
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	# def configure_permitted_parameters
+	#end
+
+end
