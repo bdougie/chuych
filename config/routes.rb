f87cff466b0145cb4df37de143ff208555cf3903
Brianllamar <ilikerobot@gmail.com>
2014-02-27 07:24:56 -0500

    added landing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Chuych::Application.routes.draw do
   resources :churches,  only: [:index, :create, :new, :show]
   resources :posts, only: [:index, :edit, :show, :new, :create]
 
-  resources :welcome, only: [:index] 
+  resources :welcome, only: [:index, :new] 
 
 
-  root :to => 'welcome#index'
+  root :to => 'welcome#new'
 
   get "church/show"
   get "church/edit"
