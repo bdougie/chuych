e5bb87c3bb5970adf915c45ede637d370829b95f
Brianllamar <ilikerobot@gmail.com>
2014-03-01 15:52:29 -0500

    add tags to table

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'fog'
 gem 'figaro'
 gem 'faker'
 gem "geocoder"
+gem 'acts-as-taggable-on'
 
 #assets
 gem 'sass-rails', '~> 4.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140301020802) do
+ActiveRecord::Schema.define(version: 20140301204651) do
 
   create_table "churches", force: true do |t|
     t.string   "name"
@@ -29,6 +29,24 @@ ActiveRecord::Schema.define(version: 20140301020802) do
     t.datetime "updated_at"
     t.integer  "user_id"
   end
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+
+  create_table "tags", force: true do |t|
+    t.string "name"
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "users", force: true do |t|
     t.string   "name"
