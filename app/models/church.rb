6ea5aedcb0cce269625c6834adc8d766004cbfdc
Brianllamar <ilikerobot@gmail.com>
2014-03-01 21:04:10 -0500

    updated tags to church model

--- app/models/church.rb
+++ app/models/church.rb
@@ -1,5 +1,6 @@
 class Church < ActiveRecord::Base
-	# attr_accessible :name, :description, :city no longer acceptable in rails 4
+	# attr_accessible :name, :description, :city, :tag_list no longer acceptable in rails 4
+	acts_as_taggable
 
 	validates :name, :city, presence: true
 	validates :description, length: { minimum: 10 }
