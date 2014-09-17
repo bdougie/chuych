c8ff5f72aee4476465844946aea1fdaa84497a39
Brian Douglas <brian.douglas@izea.com>
2014-09-17 07:55:31 -0400

    added note to write login test

--- spec/factories/visitors.rb
+++
@@ -1,6 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :visitor do
-  end
-end
--- spec/features/login_feature_spec.rb
+++ spec/features/login_feature_spec.rb
@@ -1,2 +1,20 @@
 require 'spec_helper'
 
+# user = FactoryGirl.create(:user)
+
+feature "login" do
+  pending "TODO: due to skip_confirmation not working"
+  # visit root_path
+  #  click_link 'Log In'
+  #  fill_in 'Email', with: user.email
+  #  fill_in 'Password', with: user.password
+  #  click_button 'Log In'
+  #  skip_confirmation!
+
+  #  visit root_path
+
+  #  expect(current_path).to eq root_path
+  #  expect(page).to have_content 'New user created'
+  #  expect(page).to have_content 'newuser@example.com'
+end
+
