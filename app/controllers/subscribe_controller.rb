429b6943680a9f9019988ae9a7ac2c1cacf40444
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2014-11-30 22:34:37 -0500

    removes FNAME from mailchimp

--- app/controllers/subscribe_controller.rb
+++ app/controllers/subscribe_controller.rb
@@ -8,11 +8,8 @@ class SubscribeController < ApplicationController
     @subscrition = Gibbon::API.new
     @subscrition.lists.subscribe({
       id: @list_id,
-      email: { email: subscribe_params[:email] },
-      :merge_vars => {:FNAME => 'Early Adopter'}, :double_optin => true })
+      email: { email: subscribe_params[:email] }, :double_optin => true })
 
-    # TODO create new list that does not require a name and delete :merge_vars, also create thank you page.
-    #
     redirect_to_back
   rescue Gibbon::MailChimpError => e
     return redirect_to root_path, :flash => { error: e.message }
